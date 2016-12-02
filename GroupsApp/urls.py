8cb13d4d38ea1b6269ddc9d0e223bba4cc47d61c
scott246 <scott246@purdue.edu>
2016-12-02 16:30:17 -0500

    FINALLY can add users by email
    can now add users by email to a group if you are in that group
    
    TODO: projects stuff

--- GroupsApp/urls.py
+++ GroupsApp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^group/all$', views.getGroups, name='Groups'),
 	url(r'^group/form$', views.getGroupForm, name='GroupForm'),
     url(r'^group/formsuccess$', views.getGroupFormSuccess, name='GroupFormSuccess'),
+    url(r'^group/add$', views.addMember, name='GAddMember'),
     url(r'^group/join$', views.joinGroup, name='GJoin'),
     url(r'^group/unjoin$', views.unjoinGroup, name='GUnjoin'),
     url(r'^group$', views.getGroup, name='Group'),
