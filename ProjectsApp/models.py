a14da6eb41520b303282818925f9a3c121e4055e
scott246 <scott246@purdue.edu>
2016-12-02 20:10:16 -0500

    can create projects now
    swag diggity
    
    TODO:
    - find a way to make engineers sign up for a company upon registration
    - make projects editable

--- ProjectsApp/models.py
+++ ProjectsApp/models.py
@@ -3,12 +3,36 @@
 Created by Harris Christiansen on 10/02/16.
 """
 from django.db import models
+from datetime import datetime
+from CompaniesApp.models import Company
 
 class Project(models.Model):
+    def create_project(self, name=None, description=None, created_at=str(datetime.now()), updated_at=str(datetime.now()), company=None, languages=None, yearsProgramming=None, specialty=None):
+        if not name:
+            raise ValueError('Project must have a name')
+
+        #We can safetly create the user
+        #Only the email field is required
+        self.name = name
+        self.description = description
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.company = company
+        self.languages = languages
+        self.yearsProgramming = yearsProgramming
+        self.specialty = specialty           
+        
+        self.save()
+        return self
+
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=10000)
     created_at = models.DateTimeField('date created')
     updated_at = models.DateTimeField('date updated')
+    company = models.ForeignKey('CompaniesApp.Company', default=0)
+    languages = models.CharField(max_length=200, default='NON')
+    yearsProgramming = models.CharField(max_length=3, default='0')
+    specialty = models.CharField(max_length=200, default='0')
 
     # TODO Task 3.5: Add field for company relationship
     # TODO Task 3.5: Add fields for project qualifications (minimum required: programming language, years of experience, speciality)
--- ProjectsApp/views.py
+++ ProjectsApp/views.py
@@ -3,8 +3,14 @@
 Created by Harris Christiansen on 10/02/16.
 """
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
 
 from . import models
+from .models import Project
+from CompaniesApp.models import Company
+from . import forms
+from .forms import ProjectForm
+from datetime import datetime
 
 def getProjects(request):
 	projects_list = models.Project.objects.all()
@@ -14,3 +20,38 @@ def getProjects(request):
 
 def getProject(request):
 	return render(request, 'project.html')
+
+def getProjectForm(request):
+	if request.user.is_authenticated() and request.user.usertype == 'ENG':
+		form = forms.ProjectForm()
+		return render(request, 'projectform.html', {'form': form, 'button_value': 'Submit'})
+		print request.user.usertype
+		print request.user.is_authenticated
+	return render(request, 'generalerror.html', context)
+
+def getProjectFormSuccess(request):
+	if request.user.is_authenticated() and request.user.usertype == 'ENG':
+		if request.method == 'POST':
+			form = forms.ProjectForm(request.POST or None)
+			if form.is_valid():
+				new_project = Project()
+				new_project.create_project(
+					name=form.cleaned_data['name'], 
+					description=form.cleaned_data["description"],
+					created_at=str(datetime.now()),
+					updated_at=str(datetime.now()),
+					company=models.Company.objects.get(members__exact=request.user.id),
+					languages=form.cleaned_data['languages'],
+					yearsProgramming=form.cleaned_data['yearsProgramming'],
+					specialty=form.cleaned_data['specialty'],
+					)
+				new_project.save()	
+				return render(request, 'index.html')
+
+			context = {
+				"form": form,
+			}
+		else:
+			form = forms.ProjectForm()
+	return render(request, 'generalerror.html')
+
