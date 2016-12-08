fd2af1d9088e1c1f0d17b3adf89b6bfe6ae2385d
rju <rju@purdue.edu>
2016-12-08 00:25:47 -0500

    Updated for Bookmarks App (incomplete)

--- BookmarksApp/models.py
+++ BookmarksApp/models.py
@@ -5,5 +5,15 @@ from django.db import models
 # Create your models here.
 
 class Bookmark(models.Model):
-    user_id = models.ForeignKey('AuthenticationApp.MyUser')
-    project_id = models.ForeignKey('ProjectsApp.Project')
+	def create_bookmark(self, user_id=None, project_id=None):
+#		self.user_id = models.ForeignKey('AuthenticationApp.MyUser')
+# 		self.project_id = models.ForeignKey('ProjectsApp.Project')
+# 		self.save()
+# 		return self
+		self.user_id = user_id
+		self.project_id = project_id
+		self.save()
+		return self
+		
+	user = models.ForeignKey('AuthenticationApp.MyUser', default=0)
+	project = models.ForeignKey('ProjectsApp.Project', default=0)
--- BookmarksApp/views.py
+++ BookmarksApp/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
 
+from . import models
+#from . import forms
+
+from ProjectsApp.models import Project
+
 # Create your views here.
 def getBookmarks(request):
+	bookmarked_list = models.Bookmark.objects.all()
+	# if Group.objects.get(members=request.user.id) != None:
+# 		inGroup = True
+# 	else:
+# 		inGroup = False
+		
+	context = {
+		
+		
+		'bookmarks': bookmarked_list,
+		
+	}
 	return render(request, "bookmarks.html")
+	
+#implement add bookmark function
+def addBookmark(request):
+	#need to do
+	new_bookmark = models.Bookmark()
+	new_bookmark.create_bookmark(
+		#project_id=models.ForeignKey('ProjectsApp.Project'),
+# 		project_id=request.name
+# 		need to pass project_id as a parameter but not sure how to
+		)
+	return render(request, "bookmarks.html")
+
+#implement remove bookmark function
+def removeBookmark():
+	#need to do
+	return NULL
+	
